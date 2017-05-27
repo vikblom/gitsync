b04d1fea223d00eedaf13d3db84d23b3b631c554
vikblom <veblomqvist@gmail.com>
2017-05-27 10:35:37 +0200

    Host agnostic tracked checking.

--- gitsync.sh
+++ gitsync.sh
@@ -1,25 +1,45 @@
 #!/bin/bash
 
-cd ~/signature-method/results/
-# Find out which files git are taking care of.
-GIT_LOCAL=( $(git ls-files) )
-GIT_REMOTE=( $(ssh ozzy "cd signature-method/results; git ls-files") )
+#if [[ $# -lt 2 ]]; then
+#    echo 'Usage: "gitsync host:from_path to_path"'
+#    exit 1
+#fi
+
+parse_target() {
+
+    if [[ $1 == *':'* ]] # If taget is on a different host
+    then
+        IFS=':' read -a arr <<< "$1"
+        HOST=${arr[0]}
+        DIR=${arr[1]}
+        ssh $HOST "git -C $DIR ls-files" 2> /dev/null
+    else # Assume it is on this computer
+        DIR=$1
+        git -C $DIR ls-files 2> /dev/null
+    fi
+}
+
+GIT_FROM=$( parse_target $1 )
+echo ${GIT_FROM[*]}
+
+GIT_TO=$( parse_target $2 )
+echo ${GIT_TO[*]}
 
 # Merge what git remotes locally and on remote, and remove duplicates.
-TRACKED=("${GIT_LOCAL[@]}" "${GIT_REMOTE[@]}")
+TRACKED=("${GIT_FROM[@]}" "${GIT_TO[@]}")
+echo ${TRACKED[*]}
 
-declare -A UNIQUE
+#declare -A UNIQUE
 # Store the values of TRACKED in UNIQUE as keys.
-for k in "${TRACKED[@]}"; do UNIQUE["$k"]=1; done
+#for k in "${TRACKED[@]}"; do UNIQUE["$k"]=1; done
 # Extract the keys.
-TRACKED=("${!UNIQUE[@]}")
+#TRACKED=("${!UNIQUE[@]}")
+#echo ${TRACKED[*]}
 
-printf 'Ignoring files tracked by git:\n'
-printf '%s\n' ${TRACKED[*]} ''
+#printf 'Ignoring files tracked by git:\n'
+#printf '%s\n' ${TRACKED[*]} ''
 
 # Explicityly construct string with many flags for excluding the traved files.
-EXCLUDE=("${TRACKED[@]/#/--exclude }")
-SOURCE="$OZZY:/home/exjobb_vikblom/signature-method/results/"
-rsync -avz --exclude "*.p" ${EXCLUDE[*]} $SOURCE .
-
-cd $OLDPWD
+#EXCLUDE=("${TRACKED[@]/#/--exclude }")
+#SOURCE="$OZZY:/home/exjobb_vikblom/signature-method/results/"
+#rsync -avz --exclude "*.p" ${EXCLUDE[*]} $SOURCE .
